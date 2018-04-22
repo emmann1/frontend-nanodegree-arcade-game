a37044846d55df14b674c98abd257d3e42a36f65
Emanuel M <emanuel.mares@gmail.com>
2018-04-22 18:02:17 +0300

    Added comments on the main js file

--- js/app.js
+++ js/app.js
@@ -17,12 +17,19 @@ let boardPlaces = {
         5: 405
     }
 }
+//array containing previous occupied cells by an obstacle or collectible
 let occupiedPositions = [];
+//array to hold all enemies present on the board
 let allEnemies = [];
+//array to hold all obstacles on the board
 let obstacles = [];
+//level variable
 let currentLevel = 1;
+//gameover status to end the game or restart it
 let gameOverStatus = false;
+//variable to to know when the user chosed the sprite and the game is ready to start
 let gameReady = false;
+//function to check if a cell is available to not have a collectible overlap an obstacle
 let checkBoard = function() {
     let target = this; 
     occupiedPositions.forEach(function checkElements(el) {
@@ -80,14 +87,12 @@ Enemy.prototype.update = function(dt) {
         this.x = -150;
         this.speed = Math.floor(Math.random() * 3) * defaultEnemySpeed;
     }
-    //Detect collisions
+    //Detect collisions with the player
     if(this.y == player.y && (this.x >= player.x-70 && this.x <= player.x+80)){
         //Reset player position
         player.reset();
         player.lives -= 1;
     }
-    //outputs a game over when playes has no more lives
-    
 };
 
 // Draw the enemy on the screen, required method for game
@@ -126,12 +131,11 @@ Player.prototype.reset = function() {
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    
-    
 }
 
 Player.prototype.handleInput = function(dir) {
     let target = this;
+    //get the movement direction and no to get out of bounds
     if(dir == "left" && this.col > 1) {
         this.col --;
         obstacles.forEach(function(el) {
@@ -170,28 +174,32 @@ Player.prototype.handleInput = function(dir) {
     }
 }
 
+//method that is run when the player reaches the goal and the level goes on
 Player.prototype.win = function() {
     this.score += 100;
     currentLevel ++;
+    //add an extra enemy and increase the speed by 25 every 20 levels
     if(currentLevel % 20 == 0 && allEnemies.length < 10){
         if(defaultEnemySpeed <= 200){
             defaultEnemySpeed += 25;
             allEnemies.push(createEnemy());
         }
     }
+    //add a new obstacle every 15 levels
     if(currentLevel % 15 == 0){
         if(obstacles.length < 3 ){
             obstacles.push(createObstacle());
         }
     }
     this.reset();
-    console.log("Win", defaultEnemySpeed);
 }
 
+//class for collectibles, gems and hearts
 var Collectible = function() {
     this.change();
 }
 
+//when the level is passed the collectibles are regenerated
 Collectible.prototype.change = function() {
     let randomColor = Math.floor(Math.random() * 6);
     let gemColor = ['images/Gem Orange.png', 'images/Gem Blue.png', 'images/Gem Green.png', 'images/Heart.png'];
@@ -207,29 +215,35 @@ Collectible.prototype.change = function() {
     }
 }
 
+//update method like the one from Player and Enemy classes required for the engine
 Collectible.prototype.update = function() {
     this.x = boardPlaces.column[this.col] + 20;
     this.y = boardPlaces.line[this.line] + 40;
 }
 
+//method to render the collectible
 Collectible.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y, 60,110);
 }
 
+//separate class fro obstacles
 var Obstacle = function() {
     this.sprite = 'images/Rock.png';
     this.change();
 }
 
+//method to update the position of the obstacle
 Obstacle.prototype.update = function() {
     this.x = boardPlaces.column[this.col];
     this.y = boardPlaces.line[this.line] - 10;
 }
 
+//renders the obstacle
 Obstacle.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
+//regenerates the obstacles when the level is passed
 Obstacle.prototype.change = function() {
     this.line = getRandomLine();
     this.col = getRandomCol();
@@ -241,6 +255,7 @@ Obstacle.prototype.change = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+//declase the player, initial enemies and one collectible that will change its status each level
 var player = new Player();
 let enemy1 = new Enemy(-180,boardPlaces.line[1]);
 let enemy2 = new Enemy(-250,boardPlaces.line[2]);
@@ -263,9 +278,11 @@ document.addEventListener('keydown', function(e) {
         68: 'right',
         83: 'down'
     };
+    //prevent the user to click the direction buttons when the game is over
     if(!gameOverStatus){
         player.handleInput(allowedKeys[e.keyCode]);
     }else{
+        //when the enter key is pressed the game will restart
         if(e.keyCode == 13){
             player.score = 0;
             player.reset();
